434a494cee3b2154fd39b17325f722a9b9dbb9ab
Ahmad <itxti909@gmail.com>
2025-11-25 16:23:19 +0300

    finlize local strategy

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,21 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { LocalStrategy } from './strategies/local-strategy';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly localStrategy: LocalStrategy,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
   async registerUser(@Body() body: CreateUserDto) {
     return await this.authService.registerUser(body);
   }
+
+  @UseGuards(LocalStrategy) // it first goes inside the local guard
   @Post('signin')
-  async login(@Body() body: CreateUserDto) {
-    return await this.localStrategy.validate(body);
+  async login(@Request() req) {
+    return req.user;
   }
 }
